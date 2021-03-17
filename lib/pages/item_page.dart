d42e665c2bbcc636fe1483d5bf9307d890c7ef86
annastasyaivory <annastasya2601@gmail.com>
2021-03-17 14:22:47 +0700

    Praktikum 4

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_belanja/models/item.dart';
 
 class ItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Item itemArgs = ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Shopping List"),
+      ),
+      body: Center(
+        child: Text(itemArgs.name + " price " + itemArgs.price.toString()),
       ),
     );
   }
