b99d0745042bcee380deb50706c2943aa6cff368
annastasyaivory <annastasya2601@gmail.com>
2021-03-17 14:13:34 +0700

    Praktikum 3

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Shopping List"),
+      ),
+    );
   }
 }
